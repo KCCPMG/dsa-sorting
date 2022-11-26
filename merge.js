85e7b111c8c519537ff33db328d97f3ff0ff57eb
Connor Wales <connorwales@gmail.com>
2022-11-25 16:17:30 -0800

    completed, all required tests passing

--- merge.js
+++ merge.js
@@ -1,5 +1,35 @@
-function merge() {}
+function merge(arr1, arr2) {
+  const outArr = [];
+  let pointer1 = 0;
+  let pointer2 = 0;
+  while (pointer1 < arr1.length && pointer2 < arr2.length) {
+    if (arr1[pointer1] < arr2[pointer2]) {
+      outArr.push(arr1[pointer1]);
+      pointer1++;
+    } else {
+      outArr.push(arr2[pointer2]);
+      pointer2++;
+    }
+  }
+  while (pointer1 < arr1.length) {
+    outArr.push(arr1[pointer1]);
+    pointer1++;
+  }
+  while (pointer2 < arr2.length) {
+    outArr.push(arr2[pointer2]);
+    pointer2++;
+  }
+  return outArr;
+}
 
-function mergeSort() {}
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+  // else
+  let midpoint = Math.floor(arr.length / 2);
+  let leftArr = mergeSort(arr.slice(0, midpoint));
+  let rightArr = mergeSort(arr.slice(midpoint));
+  return merge(leftArr, rightArr);
+
+}
 
 module.exports = { merge, mergeSort};
